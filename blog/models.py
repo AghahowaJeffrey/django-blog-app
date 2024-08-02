8a54e714c74f4e7a25e2dc664fa61022d1c653f4
Aghahowa Jeffrey <aghahowaj01@gmail.com>
2024-08-02 19:51:14 +0100

    feat: canonical urls, pagination, email recommendation, comment system, tagging func

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,10 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 # Create your models here.
+
+
 class PublishedManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset()\
@@ -26,6 +29,8 @@ class Post(models.Model):
 
     objects = models.Manager()
     published = PublishedManager()
+
+    tags = TaggableManager()
 
     class Meta:
         ordering = ['-publish']
